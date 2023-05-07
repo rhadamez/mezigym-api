0cdbd2b1c932cad89f9f206b3206abf982ca0909
Rhadamez Gindri Hercilio <rhadamez@gmail.com>
2023-05-07 16:10:36 -0300

    feat: refactor instances in the specs.ts

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -1,14 +1,19 @@
-import { describe, expect, test } from 'vitest'
+import { beforeEach, describe, expect, test } from 'vitest'
 import { hash } from 'bcryptjs'
 import { UsersRepositoryInMemory } from '@/repositories/in-memory/users-repository-in-memory'
 import { AuthenticationUseCase } from './authenticate'
 import { InvalidCredentialsError } from './errors/invalid-credentials-error'
 
 describe('Authenticate Use Case', () => {
-	test('should be able to authenticate', async () => {
-		const usersRepository = new UsersRepositoryInMemory()
-		const sut = new AuthenticationUseCase(usersRepository)
+	let usersRepository: UsersRepositoryInMemory
+	let sut: AuthenticationUseCase
 
+	beforeEach(() => {
+		usersRepository = new UsersRepositoryInMemory()
+		sut = new AuthenticationUseCase(usersRepository)
+	})
+
+	test('should be able to authenticate', async () => {
 		await usersRepository.create({
 			name: 'Rhadamez',
 			email: 'rhadamez@gmail.com',
@@ -24,9 +29,6 @@ describe('Authenticate Use Case', () => {
 	})
 
 	test('should not be able to authenticate with wrong email', async () => {
-		const usersRepository = new UsersRepositoryInMemory()
-		const sut = new AuthenticationUseCase(usersRepository)
-
 		await expect(sut.execute({
 			email: 'rhadamez@gmail.com',
 			password: '123321'
@@ -34,9 +36,6 @@ describe('Authenticate Use Case', () => {
 	})
 
 	test('should not be able to authenticate with wrong password', async () => {
-		const usersRepository = new UsersRepositoryInMemory()
-		const sut = new AuthenticationUseCase(usersRepository)
-
 		await usersRepository.create({
 			name: 'Rhadamez',
 			email: 'rhadamez@gmail.com',
--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,36 +1,38 @@
-import { describe, expect, test } from 'vitest'
+import { beforeEach, describe, expect, test } from 'vitest'
 import { RegisterUserCase } from './register'
 import { compare } from 'bcryptjs'
 import { UsersRepositoryInMemory } from '@/repositories/in-memory/users-repository-in-memory'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 describe('Register Use Case', () => {
-	test('should be able to register', async () => {
-		const prismaUserRepository = new UsersRepositoryInMemory()
-		const registerUseCase = new RegisterUserCase(prismaUserRepository)
+	let usersRepository: UsersRepositoryInMemory
+	let sut: RegisterUserCase
 
+	beforeEach(() => {
+		usersRepository = new UsersRepositoryInMemory()
+		sut = new RegisterUserCase(usersRepository)
+	})
+
+	test('should be able to register', async () => {
 		const userInput = {
 			name: 'rhadmaez',
 			email: 'rhadamez@hotmail1.com',
 			password: '123321'
 		}
 
-		const { user } = await registerUseCase.execute(userInput)
+		const { user } = await sut.execute(userInput)
 
 		expect(user.id).toEqual(expect.any(String))
 	})
 
 	test('check if it password hash works', async () => {
-		const prismaUserRepository = new UsersRepositoryInMemory()
-		const registerUseCase = new RegisterUserCase(prismaUserRepository)
-
 		const userInput = {
 			name: 'rhadmaez',
 			email: 'rhadamez@hotmail1.com',
 			password: '123321'
 		}
 
-		const { user } = await registerUseCase.execute(userInput)
+		const { user } = await sut.execute(userInput)
 
 		const isPasswordCorrectlyHashed = await compare('123321', user.password_hash)
 
@@ -38,17 +40,14 @@ describe('Register Use Case', () => {
 	})
 
 	test('should not be able to register with same email twice', async () => {
-		const prismaUserRepository = new UsersRepositoryInMemory()
-		const registerUseCase = new RegisterUserCase(prismaUserRepository)
-
 		const userInput = {
 			name: 'rhadmaez',
 			email: 'rhadamez@hotmail1.com',
 			password: '123321'
 		}
 
-		await registerUseCase.execute(userInput)
+		await sut.execute(userInput)
 
-		await expect(() => registerUseCase.execute(userInput)).rejects.toBeInstanceOf(UserAlreadyExistsError)
+		await expect(() => sut.execute(userInput)).rejects.toBeInstanceOf(UserAlreadyExistsError)
 	})
 })
