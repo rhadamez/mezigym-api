d7a532257040fb68effe89d703c40c9287473f6c
Rhadamez Gindri Hercilio <rhadamez@gmail.com>
2023-05-08 07:28:47 -0300

    feat: add prisma gyms repository

---
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,38 @@
+import { Prisma, Gym } from '@prisma/client'
+import { FindManyNearbyParams, GymsRepository } from '../gyms-repository'
+import { prisma } from '@/lib/prisma'
+
+export class PrismaGymsRepository implements GymsRepository {
+
+	async create(data: Prisma.GymCreateInput): Promise<Gym> {
+		return await prisma.gym.create({ data })
+	}
+
+	async findById(id: string): Promise<Gym | null> {
+		return await prisma.gym.findUnique({ where: { id } })
+	}
+
+	async findManyNearby(params: FindManyNearbyParams): Promise<Gym[]> {
+		const { latitude, longitude } = params
+		const gyms = await prisma.$queryRaw<Gym[]>`
+      SELECT * from gyms
+      WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10`
+
+		return gyms
+	}
+
+	async searchMany(query: string, page: number): Promise<Gym[]> {
+		const gyms = await prisma.gym.findMany({
+			where: {
+				title: {
+					contains: query
+				}
+			},
+			take: 20,
+			skip: (page -1 ) * 20,
+		})
+
+		return gyms
+	}
+}
+
